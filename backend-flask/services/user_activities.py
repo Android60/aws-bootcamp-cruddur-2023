09dbb4d94fc259b0b8e426697c3db02d2a740cad
Android60 <44694340+Android60@users.noreply.github.com>
2024-07-17 13:59:40 +0000

    Import xray recorder

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 segment = xray_recorder.begin_segment('user_activities')
 class UserActivities:
   def run(user_handle):
