49f746a02b1fb7d6ef0a1a19294a8367eb3a9823
Andrey Sidorov <13andry06@gmail.com>
2024-07-18 17:49:34 +0400

    Added Honeycomb instrumentation for frontend

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Honeycomb
+import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+
+const configDefaults = {
+  ignoreNetworkEvents: true,
+  // propagateTraceHeaderCorsUrls: [
+  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+  // ]
+}
+
+const sdk = new HoneycombWebSDK({
+  // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
+  debug: true, // Set to false for production environment.
+  apiKey: process.env.HONEYCOMB_API_KEY, // Replace with your Honeycomb Ingest API Key.
+  serviceName: 'frontend-react-js', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
+  instrumentations: [getWebAutoInstrumentations({
+    // Loads custom configuration for xml-http-request instrumentation.
+    '@opentelemetry/instrumentation-xml-http-request': configDefaults,
+    '@opentelemetry/instrumentation-fetch': configDefaults,
+    '@opentelemetry/instrumentation-document-load': configDefaults,
+  })],
+});
+sdk.start();
+
 const el_main = document.getElementsByTagName('main')[0];
 const root = ReactDOM.createRoot(el_main);
 root.render(
