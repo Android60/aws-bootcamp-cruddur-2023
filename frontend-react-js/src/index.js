c39df1905924be727ae4572d64d2832461bf805b
Andrey Sidorov <13andry06@gmail.com>
2024-07-19 14:16:53 +0400

    Fix Honeycomb intrumentation config

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -8,27 +8,6 @@ import reportWebVitals from './reportWebVitals';
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 
-const configDefaults = {
-  ignoreNetworkEvents: true,
-  // propagateTraceHeaderCorsUrls: [
-  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
-  // ]
-}
-
-const sdk = new HoneycombWebSDK({
-  // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
-  debug: true, // Set to false for production environment.
-  apiKey: process.env.HONEYCOMB_API_KEY, // Replace with your Honeycomb Ingest API Key.
-  serviceName: 'frontend-react-js', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
-  instrumentations: [getWebAutoInstrumentations({
-    // Loads custom configuration for xml-http-request instrumentation.
-    '@opentelemetry/instrumentation-xml-http-request': configDefaults,
-    '@opentelemetry/instrumentation-fetch': configDefaults,
-    '@opentelemetry/instrumentation-document-load': configDefaults,
-  })],
-});
-sdk.start();
-
 const el_main = document.getElementsByTagName('main')[0];
 const root = ReactDOM.createRoot(el_main);
 root.render(
@@ -41,3 +20,33 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Honeycomb
+const configDefaults = {
+  ignoreNetworkEvents: true,
+  // propagateTraceHeaderCorsUrls: [
+  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+  // ]
+}
+
+try {
+  const sdk = new HoneycombWebSDK({
+    // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
+    debug: true, // Set to false for production environment.
+    apiKey: process.env.HONEYCOMB_API_KEY, // Replace with your Honeycomb Ingest API Key.
+    serviceName: 'frontend-react-js', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
+    webVitalsInstrumentationConfig: {
+      vitalsToTrack: [],
+    },
+    instrumentations: [getWebAutoInstrumentations({
+      // Loads custom configuration for xml-http-request instrumentation.
+      '@opentelemetry/instrumentation-xml-http-request': configDefaults,
+      '@opentelemetry/instrumentation-fetch': configDefaults,
+      '@opentelemetry/instrumentation-document-load': configDefaults,
+    })],
+  });
+  sdk.start();
+}
+catch (err) {
+  console.error(err);
+}
