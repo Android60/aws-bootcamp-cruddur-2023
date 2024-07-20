77bea0bb30349b2eb64bab5b69d080d799f01a15
Andrey Sidorov <13andry06@gmail.com>
2024-07-20 11:17:00 +0400

    Fix CORS error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -91,7 +91,7 @@ cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
   expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  allow_headers=['content-type','if-modified-since','traceparent','tracesttate'],
   methods="OPTIONS,GET,HEAD,POST"
 )
 
