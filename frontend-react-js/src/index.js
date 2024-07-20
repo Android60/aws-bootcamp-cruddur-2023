799203b095582b3d65d3619fe20b28be6fc0aefc
Andrey Sidorov <13andry06@gmail.com>
2024-07-20 11:17:35 +0400

    Propogate traceparent header

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -24,16 +24,15 @@ reportWebVitals();
 // Honeycomb
 const configDefaults = {
   ignoreNetworkEvents: true,
-  // propagateTraceHeaderCorsUrls: [
-  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
-  // ]
+  propagateTraceHeaderCorsUrls: [
+  /.*127.0.0.1+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+  ]
 }
-
 try {
   const sdk = new HoneycombWebSDK({
     // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
     debug: true, // Set to false for production environment.
-    apiKey: process.env.HONEYCOMB_API_KEY, // Replace with your Honeycomb Ingest API Key.
+    apiKey: process.env.REACT_APP_HONEYCOMB_API_KEY, // Replace with your Honeycomb Ingest API Key.
     serviceName: 'frontend-react-js', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
     webVitalsInstrumentationConfig: {
       vitalsToTrack: [],
