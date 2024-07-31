6e9acb0abf9d8eee20d9473fd766f439c253c349
Andrey Sidorov <13andry06@gmail.com>
2024-07-31 19:18:13 +0400

    Move DB functions to new Db class

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -4,6 +4,7 @@ Postgres Connection Pool
 
 from psycopg_pool import ConnectionPool
 import os
+import sys
 
 def query_wrap_object(template):
   sql = f"""
@@ -21,5 +22,54 @@ def query_wrap_array(template):
   """
   return sql
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+class Db:
+  def __init__(self):
+    self.init_pool()
+
+  def init_pool():
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+
+  def query_commit():
+    try:
+      conn = self.pool.connection()
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+    except Exception as err:
+      print_psycopg_err(err)
+      # conn.rollback()
+
+  def print_psycopg_err(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print ("\npsycopg2 ERROR:", err, "on line number:", line_num)
+    print ("psycopg2 traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)
+
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+
+  def query_wrap_object(template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+
+  def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
