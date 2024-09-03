072d06c9b2232a4e2661fd69ebb2ae5bf550cd5e
Andrey Sidorov <13andry06@gmail.com>
2024-09-03 11:02:43 +0000

    Fix profile heading props

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -22,8 +22,8 @@ export default function ProfileHeading(props) {
         </div>
         <div class="info">
           <div class='id'>
-            <div className='display_name'>{props.display_name}</div>
-            <div className='handle'>@{props.handle}</div>
+            <div className='display_name'>{props.profile.display_name}</div>
+            <div className='handle'>@{props.profile.handle}</div>
           </div>
           <EditProfileButton setPopped={props.setPopped}/>
         </div>
