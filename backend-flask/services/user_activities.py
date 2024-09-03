e1b99a5bebb2d46d357c4eb8a3daec9f13548e5a
Andrey Sidorov <13andry06@gmail.com>
2024-09-03 09:58:45 +0000

    Remove debug prints

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -16,15 +16,10 @@ class UserActivities:
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      print("handle issss")
-      print(user_handle)
       sql = db.template('users','show')
       subsegment = xray_recorder.begin_subsegment('query-db')
       results = db.query_object_json(sql, {'handle':user_handle})
-      print("got results:.............")
-      print(results)
       now = datetime.now()
-
       dict = {
         "now": now.isoformat(),
         "results-size": len(results)
