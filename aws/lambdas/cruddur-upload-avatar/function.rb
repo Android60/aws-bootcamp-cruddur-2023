2bf7684d91afdd9261639ce2e10581fe70d043d3
Andrey Sidorov <13andry06@gmail.com>
2024-09-10 10:39:15 +0000

    Added CORS headers

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -9,5 +9,13 @@ def lambda_handler(event:, context:)
   obj = s3.bucket(bucket_name).object(object_key)
   url = obj.presigned_url(:put, expires_in: 300)
   body = {url: url}.to_json
-  return { statusCode: 200, body:body}
+  return { 
+    statusCode: 200,
+    body:body,
+    headers: {
+      "Access-Control-Allow-Headers": "*, Authorization",
+      "Access-Control-Allow-Origin": "http://127.0.0.1:3000",
+      "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+    }
+  }
 end
