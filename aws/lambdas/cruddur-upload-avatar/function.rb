948642fc7c9ee900af86ce2320f4f1d8f161d19a
Andrey Sidorov <13andry06@gmail.com>
2024-09-11 08:30:18 +0000

    Support CORS, get sub from lambda authorizer

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -3,6 +3,20 @@ require 'json'
 
 def lambda_handler(event:, context:)
   puts event
+  if event['routeKey'] == "OPTIONS /{proxy+}"
+    puts "This is OPTIONS!!!"
+    return { 
+      statusCode: 200,
+      body: JSON.generate('Hello from cruddur-cors!'),
+      headers: {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "http://127.0.0.1:3000",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+      }
+    }
+  end
+  
+  sub = event['requestContext']['authorizer']['lambda']['sub']
   s3 = Aws::S3::Resource.new
   bucket_name = ENV["UPLOADS_BUCKET_NAME"]
   object_key = 'mock.jpg'
