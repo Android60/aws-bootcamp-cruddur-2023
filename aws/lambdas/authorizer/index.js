a624443289af6f7971902c446ad5d387260e739b
Andrey Sidorov <13andry06@gmail.com>
2024-09-11 08:32:09 +0000

    Fix jwt format, save sub to context

--- aws/lambdas/authorizer/index.js
+++ aws/lambdas/authorizer/index.js
@@ -13,11 +13,14 @@ const jwtVerifier = CognitoJwtVerifier.create({
 
 exports.handler = async (event) => {
   console.log("request:", JSON.stringify(event, undefined, 2));
-
-  const jwt = event.headers.authorization;
+  let sub = ""
+  const auth = event.headers.authorization;
+  const jwt = auth.replace("Bearer ","");
   try {
+    console.log("Got jwt: ", jwt);
     const payload = await jwtVerifier.verify(jwt);
     console.log("Access allowed. JWT payload:", payload);
+    sub = payload.sub;
   } catch (err) {
     console.error("Access forbidden:", err);
     return {
@@ -26,5 +29,8 @@ exports.handler = async (event) => {
   }
   return {
     isAuthorized: true,
+    context: {
+      sub: sub
+    }
   };
 };
