319f62e2cb1513bd0fe1efbd28f2d43170fd2a3b
Andrey Sidorov <13andry06@gmail.com>
2024-09-23 11:42:52 +0000

    Install cfn-lint in devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt install -y postgresql-client
 pip3 install -r backend-flask/requirements.txt
+pip3 install cfn-lint
 npm install aws-cdk -g
 cd thumbing-serverless-cdk
 npm i
