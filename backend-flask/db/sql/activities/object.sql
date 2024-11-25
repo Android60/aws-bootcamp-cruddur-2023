e8a9932f3b4500b1efa8be21df9d34e48b260f6c
Andrey Sidorov <13andry06@gmail.com>
2024-11-25 15:03:32 +0000

    Added reply_to_activity_uuid to object sql template

--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -4,7 +4,8 @@ SELECT
     users.display_name,
     activities.message,
     activities.created_at,
-    activities.expires_at
+    activities.expires_at,
+    activities.reply_to_activity_uuid 
 FROM public.activities
 INNER JOIN public.users on users.uuid = activities.user_uuid
 WHERE
