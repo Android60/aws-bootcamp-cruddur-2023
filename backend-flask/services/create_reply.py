aeda06bff84285b13e672142f8dd68f7dadc56d6
Andrey Sidorov <13andry06@gmail.com>
2024-11-27 13:10:02 +0000

    Pass cognito_user_id, not handle

--- backend-flask/services/create_reply.py
+++ backend-flask/services/create_reply.py
@@ -3,14 +3,14 @@ from datetime import datetime, timedelta, timezone
 from lib.db import db
 
 class CreateReply:
-  def run(message, user_handle, activity_uuid):
+  def run(message, cognito_user_id, activity_uuid):
     model = {
       'errors': None,
       'data': None
     }
 
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['user_handle_blank']
+    if cognito_user_id == None or len(cognito_user_id) < 1:
+      model['errors'] = ['cognito_user_id_blank']
 
     if activity_uuid == None or len(activity_uuid) < 1:
       model['errors'] = ['activity_uuid_blank']
@@ -27,14 +27,14 @@ class CreateReply:
         'reply_to_activity_uuid': activity_uuid
       }
     else:
-      uuid = CreateReply.create_reply(user_handle, activity_uuid, message)
+      uuid = CreateReply.create_reply(cognito_user_id, activity_uuid, message)
       object_json = CreateReply.query_object_activity(uuid)
       model['data'] = object_json
     return model
 
-  def create_reply(handle, activity_id, message):
+  def create_reply(cognito_user_id, activity_id, message):
     sql = db.template('activities','reply')
-    uuid = db.query_commit(sql, {'handle':handle, 'reply_to_activity_uuid':activity_id, 'message':message})
+    uuid = db.query_commit(sql, {'cognito_user_id':cognito_user_id, 'reply_to_activity_uuid':activity_id, 'message':message})
     return uuid
   
   def query_object_activity(uuid):
