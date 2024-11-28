2fdbdb7dc304755fb5c40cb70a4eae10a2a9f648
Andrey Sidorov <13andry06@gmail.com>
2024-11-28 10:36:08 +0000

    Added filler for empty activity feed

--- frontend-react-js/src/components/ActivityFeed.js
+++ frontend-react-js/src/components/ActivityFeed.js
@@ -2,11 +2,18 @@ import './ActivityFeed.css';
 import ActivityItem from './ActivityItem';
 
 export default function ActivityFeed(props) {
+  if (props.activities.length === 0){
+    return (
+    <div className='activity_feed_primer'>
+      <span>It's so empty!</span>
+    </div>
+    );
+  } else {
   return (
     <div className='activity_feed_collection'>
       {props.activities.map(activity => {
       return  <ActivityItem setReplyActivity={props.setReplyActivity} setPopped={props.setPopped} key={activity.uuid} activity={activity} />
       })}
     </div>
-  );
+  );}
 }
